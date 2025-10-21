78cd4e44ca235ec1f8e4114a6e96a9c3f30d0afa
Tammy Ma <ma.wenjing1@northeastern.edu>
2025-10-20 20:37:32 -0700

    Implement strategy pattern

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -9,62 +9,65 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
+class UpdateStrategy:
+    def update(self, item):
+        raise NotImplementedError
+
+    @staticmethod
+    def _inc_quality(item, n=1):
+        item.quality = min(50, item.quality + n)
+
+    @staticmethod
+    def _dec_quality(item, n=1):
+        item.quality = max(0, item.quality - n)
+
+class DefaultStrategy(UpdateStrategy):
+    def update(self, item):
+        item.sell_in -= 1
+        self._dec_quality(item, 1)
+        if item.sell_in < 0:
+            self._dec_quality(item, 1)
+
+class AgedBrieStrategy(UpdateStrategy):
+    def update(self, item):
+        item.sell_in -= 1
+        self._inc_quality(item, 1)
+        if item.sell_in < 0:
+            self._inc_quality(item, 1)
+
+class BackstageStrategy(UpdateStrategy):
+    def update(self, item):
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = 0
+            return
+
+        if item.sell_in < 5:
+            self._inc_quality(item, 3)
+        elif item.sell_in < 10:
+            self._inc_quality(item, 2)
+        else:
+            self._inc_quality(item, 1)
+
+class SulfurasStrategy(UpdateStrategy):
+    def update(self, item):
+        pass
 
 class GildedRose(object):
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
     AGED_BRIE = "Aged Brie"
     BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
-    SULFURAS = "Sulfuras, Hand of Ragnaros"
+
+    STRATEGIES = {
+        AGED_BRIE: AgedBrieStrategy(),
+        BACKSTAGE: BackstageStrategy(),
+        SULFURAS: SulfurasStrategy(),
+    }
 
     def __init__(self, items):
         self.items = items
 
-    @staticmethod
-    def _inc_quality(item, by=1):
-        item.quality = min(50, item.quality + by)
-
-    @staticmethod
-    def _dec_quality(item, by=1):
-        item.quality = max(0, item.quality - by)
-
-    @staticmethod
-    def _dec_sell_in(item):
-        item.sell_in -= 1
-
-    def _update_sulfuras(self, item):
-        return
-
-    def _update_aged_brie(self, item):
-        self._inc_quality(item, 1)
-        self._dec_sell_in(item)
-        if item.sell_in < 0:
-            self._inc_quality(item, 1)
-
-    def _update_backstage(self, item):
-        if item.sell_in > 10:
-            self._inc_quality(item, 1)
-        elif item.sell_in > 5:
-            self._inc_quality(item, 2)
-        elif item.sell_in >= 0:
-            self._inc_quality(item, 3)
-
-        self._dec_sell_in(item)
-
-        if item.sell_in < 0:
-            item.quality = 0
-
-    def _update_default(self, item):
-        self._dec_quality(item, 1)
-        self._dec_sell_in(item)
-        if item.sell_in < 0:
-            self._dec_quality(item, 1)
-
     def update_quality(self):
         for item in self.items:
-            if item.name == self.SULFURAS:
-                self._update_sulfuras(item)
-            elif item.name == self.AGED_BRIE:
-                self._update_aged_brie(item)
-            elif item.name == self.BACKSTAGE:
-                self._update_backstage(item)
-            else:
-                self._update_default(item)
+            strategy = self.STRATEGIES.get(item.name, DefaultStrategy())
+            strategy.update(item)
